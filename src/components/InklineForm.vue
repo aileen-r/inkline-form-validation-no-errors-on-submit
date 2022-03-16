a0a1893240f3ab92e5bb1fd9c291b5a878512410
Aileen Rae <aileenlrae@gmail.com>
2022-03-16 16:52:00 +0000

    Write better code comments

--- src/components/InklineForm.vue
+++ src/components/InklineForm.vue
@@ -1,22 +1,25 @@
 <template>
   <h1>Inkline Form Errors</h1>
-  <p>Try sumbitting the empty form.</p>
+  <p>Try submitting the empty form.</p>
   <i-form v-model="form" @submit="submit">
     <i-form-group>
       <i-form-label for="email">Email Address</i-form-label>
       <i-input name="email" type="email" />
-      <i-form-error for="email" />
+      <i-form-error for="email" :visible="['invalid', 'touched']" />
     </i-form-group>
     <i-form-group>
       <i-form-label for="password">Password</i-form-label>
       <i-input name="password" type="password" />
-      <i-form-error for="password" />
+      <i-form-error for="password" :visible="['invalid', 'touched']" />
     </i-form-group>
     <i-form-group>
       <i-button block color="primary" type="submit">Log In</i-button>
     </i-form-group>
   </i-form>
-  <p>Both of these inputs are required, blocking the form submit, but no error feedback is shown.</p>
+  <p>
+    Both of these inputs are required, blocking the form submit, but no error
+    feedback is shown.
+  </p>
 </template>
 
 <script>
@@ -47,11 +50,14 @@ export default {
     };
   },
   methods: {
-    submit() {
-      // I would expect this to be called, making the error messages appear.
-      this.form.dirty = true;
-    }
-  }
+    submit(payload) {
+      // I would expect this to be called when clicking "log in"
+      if (payload.invalid) {
+        // Then I could set fields to touched so that error messages appear
+        this.form.touched = true;
+      }
+    },
+  },
 };
 </script>
 
